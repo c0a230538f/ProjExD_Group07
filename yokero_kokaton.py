3ab0ae1ae6bcec1a51f64e979c6fce9da99b0d20
Yuya Kubodera <130113655+c0a230538f@users.noreply.github.com>
2024-12-23 18:34:06 +0900

    機能追加
    クリープ現象の追加と、メーターのサイズを変更しやすいように修正

--- yokero_kokaton.py
+++ yokero_kokaton.py
@@ -14,43 +14,44 @@ class Speedometer:
         self.sizey = sizey # ウィンドウの高さを設定
         self.meter_x = sizex - sizex / 7 # メーターのx座標を設定
         self.meter_y = sizey - sizey / 11 # メーターのy座標を設定
-        self.meter_radius = sizey / 12 # メーターの半径を設定
+        self.meter_radius = 100 # メーターの半径を設定
 
     def draw(self, sokudo:float):
+        meter_huti = self.meter_radius//10 # メーターの縁の太さ
         meter_angle = -45 + (sokudo / 150) * 180  # 速度に応じた角度（0-150km/hを180-45度の位置に変換）
-        # メーターの背景円を描画
-        pg.draw.circle(self.screen, (50, 50, 50), (self.meter_x, self.meter_y), self.meter_radius, 4) # メーターの背景円を描画
+        # メーターの縁を描画
+        pg.draw.circle(self.screen, (50, 50, 50), (self.meter_x, self.meter_y), self.meter_radius, meter_huti) # メーターの背景円を描画
         # メーターの内側を透明にする
         transparent_surface = pg.Surface((self.meter_radius * 2, self.meter_radius * 2), pg.SRCALPHA) # 透明なサーフェスを作成
-        pg.draw.circle(transparent_surface, (255, 165, 0, 50), (self.meter_radius, self.meter_radius), self.meter_radius - 3) # 透明な円を描画
+        pg.draw.circle(transparent_surface, (0, 0, 10, 50), (self.meter_radius, self.meter_radius), self.meter_radius - meter_huti) # 透明な円を描画
         self.screen.blit(transparent_surface, (self.meter_x - self.meter_radius+1, self.meter_y - self.meter_radius + 1)) # 透明な円をスクリーンに描画
-        # メーターの内側円を描画
-        pg.draw.circle(self.screen, (255, 0, 0), (self.meter_x, self.meter_y), self.meter_radius-5, 1) # メーターの内側円を描画
+        # メーターの内側円を描画(デザイン用)
+        pg.draw.circle(self.screen, (100, 100, 255), (self.meter_x, self.meter_y), self.meter_radius - meter_huti + 1, self.meter_radius//30) # メーターの内側円を描画
         # メーターのメモリを描画
-        for i in range(-45, 226, 24):  # 0度から180度まで24度刻みでメモリを描画
+        for i in range(-44, 226, 24):  # 0度から180度まで24度刻みでメモリを描画
             angle = 180 - i # 角度を計算
-            start_x = self.meter_x + (self.meter_radius - 5) * math.cos(math.radians(angle)) # メモリの始点のx座
-            start_y = self.meter_y - (self.meter_radius - 5) * math.sin(math.radians(angle)) # メモリの始点のy座標
-            end_x = self.meter_x + (self.meter_radius - 13) * math.cos(math.radians(angle)) # メモリの終点のx座標
-            end_y = self.meter_y - (self.meter_radius - 13) * math.sin(math.radians(angle)) # メモリの終点のy座標
+            start_x = self.meter_x + (self.meter_radius - meter_huti - self.meter_radius//20) * math.cos(math.radians(angle)) # メモリの始点のx座標
+            start_y = self.meter_y - (self.meter_radius - meter_huti - self.meter_radius//20) * math.sin(math.radians(angle)) # メモリの始点のy座標
+            end_x = self.meter_x + (self.meter_radius - meter_huti - self.meter_radius//6) * math.cos(math.radians(angle)) # メモリの終点のx座標
+            end_y = self.meter_y - (self.meter_radius - meter_huti - self.meter_radius//6) * math.sin(math.radians(angle)) # メモリの終点のy座標
             pg.draw.line(self.screen, (255, 255, 255), (start_x, start_y), (end_x, end_y), 2) # メモリを描画
         # メーターの細かなメモリを描画
-        for i in range(-45, 225, 6):  # 0度から180度まで6度刻みで細かなメモリを描画
+        for i in range(-44, 225, 6):  # 0度から180度まで6度刻みで細かなメモリを描画
             angle = 180 - i # 角度を計算
-            start_x = self.meter_x + (self.meter_radius - 5) * math.cos(math.radians(angle)) # メモリの始点のx座標
-            start_y = self.meter_y - (self.meter_radius - 5) * math.sin(math.radians(angle)) # メモリの始点のy座標
-            end_x = self.meter_x + (self.meter_radius - 10) * math.cos(math.radians(angle)) # メモリの終点のx座標
-            end_y = self.meter_y - (self.meter_radius - 10) * math.sin(math.radians(angle)) # メモリの終点のy座標
+            start_x = self.meter_x + (self.meter_radius - meter_huti - self.meter_radius//20) * math.cos(math.radians(angle)) # メモリの始点のx座標
+            start_y = self.meter_y - (self.meter_radius - meter_huti - self.meter_radius//20) * math.sin(math.radians(angle)) # メモリの始点のy座標
+            end_x = self.meter_x + (self.meter_radius - meter_huti - self.meter_radius//10) * math.cos(math.radians(angle)) # メモリの終点のx座標
+            end_y = self.meter_y - (self.meter_radius - meter_huti - self.meter_radius//10) * math.sin(math.radians(angle)) # メモリの終点のy座標
             pg.draw.line(self.screen, (255, 255, 255), (start_x, start_y), (end_x, end_y), 1) # メモリを描画
         # メーターの針を描画
-        needle_length = self.meter_radius - 11 # 針の長さ
+        needle_length = self.meter_radius * 0.8 # 針の長さ
         needle_x = self.meter_x + needle_length * math.cos(math.radians(180 - meter_angle)) # 針の先端のx座標
         needle_y = self.meter_y - needle_length * math.sin(math.radians(180 - meter_angle)) # 針の先端のy座標
         pg.draw.line(self.screen, (255, 0, 0), (self.meter_x, self.meter_y), (needle_x, needle_y), 4) # 針を描画
         # メーターの中心に透明な小さい円を描画
         transparent_center = pg.Surface((self.meter_radius//5, self.meter_radius//5), pg.SRCALPHA) # 透明なサーフェスを作成
         pg.draw.circle(transparent_center, (0, 0, 0, 200), (self.meter_radius//10, self.meter_radius//10), self.meter_radius//10) # 透明な円を描画
-        self.screen.blit(transparent_center, (self.meter_x - self.meter_radius//10, self.meter_y - self.meter_radius//13)) # 透明な円をスクリーンに描画
+        self.screen.blit(transparent_center, (self.meter_x - self.meter_radius//10 + self.meter_radius//40, self.meter_y - self.meter_radius//13)) # 透明な円をスクリーンに描画
         # 速度計の数値をメーター内に表示
         font = pg.font.Font(None, int(self.meter_radius/3))  # 速度計のフォント
         text = font.render(f"{int(sokudo)}km/h", True, (255, 255, 255))  # 速度計の文字列を作成
@@ -86,6 +87,9 @@ def cal_speed(sokudo:float, energy:float):
         energy -= (10*((1.014)**sokudo))-10
         if energy < 0:
             energy = 0
+    #車の速度が12km/h以下の場合は、クリープ現象を再現する
+    if sokudo < 12.3:
+        energy += random.randint(0, 4)
     #車の持つエネルギーを速度に変換する
     sokudo = math.sqrt(energy)/4
     if sokudo < 0:
@@ -116,7 +120,7 @@ def main():
         if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
             energy += cal_power(sokudo, power) #車の持つエネルギーを加算し続ける
         elif key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
-            energy -= cal_power(sokudo, power)*3 #車の持つエネルギーを減らす
+            energy -= cal_power(sokudo, power)*2 #車の持つエネルギーを減らす
             if energy < 0:
                 energy = 0
 
